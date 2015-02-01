4e00c8ed865d0a5e099cb586a2a3cb1c1d5979c6
Alexis Leroux-Chartre <alexis.leroux.chartre@gmail.com>
2015-02-01 16:08:46 -0500

    Material-ui it is

--- app/scripts/pages/CreateQuest.js
+++ app/scripts/pages/CreateQuest.js
@@ -1,9 +1,50 @@
 var React = require('react');
+var mui = require('material-ui');
 
 module.exports = React.createClass({
-    render: () => {
+    getInitialState() {
+        return {
+            id: '',
+            name: '',
+            description: '',
+            experience: 10,
+
+            latitude: 45.5,
+            longitude: -73.56,
+
+            objectives: [],
+
+            creator: '',
+
+            isPublic: false,
+
+            createdOn: '',
+            finishedOn: ''
+        };
+    },
+    render() {
         return (
-            <div>Hello Quest</div>
+            <form>
+                <h4>Create a quest</h4>
+                <div>
+                    <mui.TextField floatingLabelText='Quest Name' value={this.state.name} onChange={this.handleChange.bind(this, 'name')}/>
+                </div>
+                <div>
+                    <mui.TextField floatingLabelText='Quest Description' value={this.state.description} onChange={this.handleChange.bind(this, 'description')}/>
+                </div>
+                <div>
+                    <mui.TextField floatingLabelText='Latitude' value={this.state.latitude} onChange={this.handleChange.bind(this, 'latitude')}/>
+                </div>
+                <div>
+                    <mui.TextField floatingLabelText='longitude' value={this.state.longitude} onChange={this.handleChange.bind(this, 'longitude')}/>
+                </div>
+            </form>
         );
+    },
+    handleChange(propName, e) {
+        var state = {};
+        state[propName] = e.target.value;
+
+        this.setState(state);
     }
 });
