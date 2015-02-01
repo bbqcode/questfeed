a3e131e479b39544192378cd3230b729046853a6
Alexis Leroux-Chartre <alexis.leroux.chartre@gmail.com>
2015-02-01 11:20:48 -0500

    Simple React boilerplate code

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000',
+    'webpack/hot/only-dev-server',
+    './scripts/main'
+  ],
+  output: {
+    path: __dirname + '/scripts/',
+    filename: 'bundle.js',
+    publicPath: '/scripts/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['react-hot', 'jsx?harmony'] },
+      { test: /\.jsx$/, loaders: ['react-hot', 'jsx?harmony'] }
+    ]
+  }
+}
