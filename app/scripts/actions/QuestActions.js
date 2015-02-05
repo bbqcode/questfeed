a5fd30aec626f0f48ad3472eb39fa689b601a535
Alexis Leroux-Chartre <alexis.leroux.chartre@gmail.com>
2015-02-04 23:03:02 -0500

    Clean feed initial data

--- app/scripts/actions/QuestActions.js
+++ app/scripts/actions/QuestActions.js
@@ -1,4 +1,17 @@
 var Reflux = require('reflux');
+var uuid = require('node-uuid');
+
+var DUMMY_DATA = { };
+
+(() => {
+    for (var i = 0; i < 10; i++) {
+        var id = uuid.v4();
+        DUMMY_DATA[id] = {
+            id: id,
+            name: 'Quest name ' + i + '!!!'
+        };
+    }
+})();
 
 var QuestActions = Reflux.createActions({
     'getQuest': { asyncResult: true },
@@ -16,39 +29,21 @@ QuestActions.getQuest.listen((id) => {
 });
 
 QuestActions.createQuest.listen((quest) => {
-    QuestActions.createQuest.failed({
-        message: 'Nope.js'
-    });
+    if (!quest || !quest.name) {
+        QuestActions.createQuest.failed({
+            message: 'Nope.js'
+        });
+    } else {
+        quest.id = uuid.v4();
+        // Add quest inside the fake map object
+        DUMMY_DATA[quest.id] = quest;
+
+        QuestActions.createQuest.completed(quest);
+    }
 });
 
 QuestActions.getQuests.listen(() => {
-    var FAKEDATA = {
-        'ID1': {
-            id: "ID1",
-            name: 'ID1 The quest of Tests'
-        },
-        'ID2': {
-            id: "ID2",
-            name: 'ID2 The quest of Tests'
-        },
-        'ID3': {
-            id: "ID3",
-            name: 'ID3 The quest of Tests'
-        },
-        'ID4': {
-            id: "ID4",
-            name: 'ID4 The quest of Tests'
-        },
-        'ID5': {
-            id: "ID5",
-            name: 'ID5 The quest of Tests'
-        },
-        'ID6': {
-            id: "ID6",
-            name: 'ID6 The quest of Tests'
-        }
-    };
-    QuestActions.getQuests.completed(FAKEDATA);
+    QuestActions.getQuests.completed(DUMMY_DATA);
 });
 
 module.exports = QuestActions;
