2229c37906e8b900dcc7e7b2cbe1759e07c2c6be
Alexis Leroux-Chartre <alexis.leroux.chartre@gmail.com>
2015-02-04 20:20:22 -0500

    Use foundation form

--- app/scripts/pages/CreateQuest.js
+++ app/scripts/pages/CreateQuest.js
@@ -16,7 +16,7 @@ module.exports = React.createClass({
 
             creator: '',
 
-            isPublic: false,
+            isPublic: true,
 
             createdOn: '',
             finishedOn: ''
@@ -24,27 +24,65 @@ module.exports = React.createClass({
     },
     render() {
         return (
-            <form>
+            <form action="/quests/create" method="post" onSubmit={this.handleSubmit}>
                 <h4>Create a quest</h4>
-                <div>
-                    <mui.TextField floatingLabelText='Quest Name' value={this.state.name} onChange={this.handleChange.bind(this, 'name')}/>
+                <div className="row">
+                    <div className="large-12 columns">
+                        <label>Quest Name
+                            <input type="text" placeholder="Quest Name" value={this.state.name} onChange={this.handleChange.bind(this, 'name')}/>
+                        </label>
+                    </div>
                 </div>
-                <div>
-                    <mui.TextField floatingLabelText='Quest Description' value={this.state.description} onChange={this.handleChange.bind(this, 'description')}/>
+                <div className="row">
+                    <div className="large-12 columns">
+                        <label>Quest Description
+                            <textarea placeholder="Quest Description" value={this.state.description} onChange={this.handleChange.bind(this, 'description')}/>
+                        </label>
+                    </div>
                 </div>
-                <div>
-                    <mui.TextField floatingLabelText='Latitude' value={this.state.latitude} onChange={this.handleChange.bind(this, 'latitude')}/>
+                <div className="row">
+                    <div className="small-6 columns">
+                        <label>Latitude
+                            <input type="text" placeholder="Latitude" value={this.state.latitude} onChange={this.handleChange.bind(this, 'latitude')}/>
+                        </label>
+                    </div>
+                    <div className="small-6 columns">
+                        <label>Longitude
+                            <input type="text" placeholder="Longitude" value={this.state.longitude} onChange={this.handleChange.bind(this, 'longitude')}/>
+                        </label>
+                    </div>
                 </div>
-                <div>
-                    <mui.TextField floatingLabelText='longitude' value={this.state.longitude} onChange={this.handleChange.bind(this, 'longitude')}/>
+                <div className="row">
+                    <div className="large-12 columns">
+                        <input id="cq-isPublic" type="checkbox" checked={this.state.isPublic} onChange={this.handleChange.bind(this, 'isPublic')}/><label htmlFor="cq-isPublic">Is public</label>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="large-12 columns">
+                        <ul className="button-group right">
+                            <li><button className="button small secondary">Clear</button></li>
+                            <li><button className="button small success" type="submit">Save</button></li>
+                        </ul>
+                    </div>
                 </div>
             </form>
         );
     },
     handleChange(propName, e) {
         var state = {};
-        state[propName] = e.target.value;
+        var newVal = null;
+
+        if (e.target.type === 'checkbox') {
+            newVal = e.target.checked;
+        } else {
+            newVal = e.target.value;
+        }
+        state[propName] = newVal;
 
         this.setState(state);
+    },
+    handleSubmit: function () {
+
     }
 });
